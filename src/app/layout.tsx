d8554cd10839f1fe1ca644f71b97092a100bb8f4
win <hoangnhat.contact@gmail.com>
2026-02-06 16:03:22 +0700

    feat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,18 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "./query-provider";
 
-const montserratSans = Montserrat({
+const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
 });
 
-const montserratMono = Montserrat({
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -31,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${montserratSans.variable} ${montserratMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>
           <TooltipProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white text-slate-900 flex items-center justify-center px-6 py-10 sm:px-10 font-[family-name:var(--font-geist-sans)]">
-      <main className="w-full max-w-2xl rounded-3xl bg-white shadow-xl shadow-slate-200/80 px-6 py-8 sm:px-10 sm:py-4 flex flex-col items-center sm:items-start">
-        <div className="inline-flex items-center justify-center rounded-xl bg-white px-4 py-3 self-center">
-          <Image
-            className="h-auto w-auto"
-            src="/Guru_no_text.png"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-        </div>
-        <div className="text-base sm:text-lg leading-relaxed text-center sm:text-left font-[family-name:var(--font-geist-mono)] text-slate-600 space-y-4">
-              - Start by describing how your team works day to day 
-              <br />
-              <br />
-              - Or upload the spreadsheets you already use.
-        </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+       
+        <span>Welcome. Let’s build your Operating System.
 
-      
-      </main>
-    
+Start by describing:
+	•	how your team works day to day
+	•	or upload the spreadsheets you already use	
+
+GURU will guide you, ask the right questions,
+and turn your requests into software.</span>
     </div>
   );
 }
